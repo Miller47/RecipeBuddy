06f4faeda68d70eb3ac9252bc8230762baeb5c0a
Tyler Miller <tyemiller4@gmail.com>
2014-11-07 16:13:20 -0500

    Navigation Drawer and UI Work

--- RecipeBuddy/app/src/main/java/com/miller/tyler/recipebuddy/MainActivity.java
+++ RecipeBuddy/app/src/main/java/com/miller/tyler/recipebuddy/MainActivity.java
@@ -1,36 +1,98 @@
 package com.miller.tyler.recipebuddy;
 
 import android.app.Activity;
+import android.content.res.Configuration;
 import android.os.Bundle;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v4.app.ActionBarDrawerToggle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
 
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements AdapterView.OnItemClickListener {
+
+    private DrawerLayout mDrawerLayout;
+    private ListView mDrawerList;
+    private String[] mBrowseList;
+    private ActionBarDrawerToggle mDrawerListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
+
+        mDrawerLayout =  (DrawerLayout) findViewById(R.id.drawer_layout);
+        mDrawerListener = new ActionBarDrawerToggle(this, mDrawerLayout, R.drawable.ic_drawer, R.string.drawer_open, R.string.drawer_closed) {
+            @Override
+            public void onDrawerClosed(View drawerView) {
+                super.onDrawerClosed(drawerView);
+            }
+
+            @Override
+            public void onDrawerOpened(View drawerView) {
+                super.onDrawerOpened(drawerView);
+            }
+        };
+        mDrawerLayout.setDrawerListener(mDrawerListener);
+        getActionBar().setHomeButtonEnabled(true);
+        getActionBar().setDisplayHomeAsUpEnabled(true);
 
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
+
+
+        // Set up array for drawer list
+        mBrowseList = getResources().getStringArray(R.array.browse_by);
+        // Set up drawer list
+        mDrawerList = (ListView) findViewById(R.id.left_drawer_list);
+        mDrawerList.setAdapter(new ArrayAdapter<String>(this, R.layout.drawer_row_layout, R.id.row_item, mBrowseList));
+
+        mDrawerList.setOnItemClickListener(this);
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
+
+        if (mDrawerListener.onOptionsItemSelected(item)) {
             return true;
         }
         return super.onOptionsItemSelected(item);
+
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        mDrawerListener.onConfigurationChanged(newConfig);
+    }
+
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        mDrawerListener.syncState();
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+        Toast.makeText(this, mBrowseList[position]+ "Selected ", Toast.LENGTH_LONG).show();
+        selectItem(position);
+
+    }
+
+    public void selectItem(int position) {
+
+        mDrawerList.setItemChecked(position, true);
+        setTitle(mBrowseList[position]);
+
+    }
+
+    public void setTitle(String title) {
+
+        getActionBar().setTitle(title);
     }
 }
